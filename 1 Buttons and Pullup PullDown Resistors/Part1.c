c74b80f9e15c6d8f3e847fa71060113f2cbe3a5a
Luciano Miletta <milett56@students.rowan.edu>
2023-02-02 17:02:52 -0500

    Complete
    Turn off Green LED and Red Led in the beginning and then invert the bit so there is a 0 for the buttons to begin with.

--- 1 Buttons and Pullup PullDown Resistors/Part1.c
+++ 1 Buttons and Pullup PullDown Resistors/Part1.c
@@ -2,7 +2,7 @@
  *  Pull-Up Resistor Configuration
  *
  *  Created on: Jan 31, 2023
- *      Author: Russell
+ *      Author: Luciano Miletta
  *      Version: 1.0
  *
  *      This example will show you how to configure the Pull-up Resistor for your button inputs.
@@ -28,13 +28,15 @@ int main(void)
 
     PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
                                             // to activate previously configured port settings
-
+    P6OUT &= ~BIT6;          // Toggle P6.6
+    P1OUT &= ~BIT0;          // Toggle P1.0
 
     while(1)
+
     {
-        if (P2IN & BIT3)            // If S2 (P2.3) is pressed
+        if (!(P2IN & BIT3))            // If S2 (P2.3) is pressed
             P6OUT ^= BIT6;          // Toggle P6.6
-        if (P4IN & BIT1)            // If S1 (P4.1) is pressed
+        if (!(P4IN & BIT1))            // If S1 (P4.1) is pressed
             P1OUT ^= BIT0;          // Toggle P1.0
         __delay_cycles(100000);             // Delay for 100000*(1/MCLK)=0.1s
     }
